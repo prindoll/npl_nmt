e2cb29695f7f49496842a8b4f027c4c428266d77
prindoll <22021209@vnu.edu.vn>
2025-05-08 18:37:50 +0700

    fix: fix error size input_vocab

--- modules/inference/__init__.py
+++ modules/inference/__init__.py
@@ -2,9 +2,11 @@ from modules.inference.decode_strategy import DecodeStrategy
 from modules.inference.beam_search import BeamSearch
 from modules.inference.prototypes import BeamSearch2
 from modules.inference.sampling_temperature import GreedySearch
+from modules.inference.topksampling import TopKSampling
 
 strategies = {
         "BeamSearch": BeamSearch,
         "BeamSearch2": BeamSearch2,
-        "GreedySearch": GreedySearch
+        "GreedySearch": GreedySearch,
+        "TopKSampling": TopKSampling,
 }
